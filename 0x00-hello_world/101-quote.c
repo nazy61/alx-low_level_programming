55da0b835adc3ca05d8595cc6df1115548c161ca
Ogwo Chinaza <ogwochinaza@yahoo.com>
2022-03-12 01:55:35 +0100

    updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - Entry point
  *
  * Return: Always 1 (Error)
-*/
+ */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\"");
-fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
-return (0);
+    fprintf(stderr, "and that piece of art is useful\"");
+    fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+    return (0);
 }
