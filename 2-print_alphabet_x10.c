55ba91b6964bff31c39262ce2b01b0c5f3c9a542
Ogwo Chinaza <ogwochinaza@yahoo.com>
2022-03-16 03:28:49 +0100

    updates

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10  - A function that prints the alphabets 10 times
+ *
+ * Description: Funtion that prints the alphabets
+ * 10 times and a newline after each
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
